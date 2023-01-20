0f9e9af23c266b3310ec271f3fbec46280c02a26
anarobertaandrade <ana48945726@edum.rn.senac.br>
2023-01-20 10:10:44 -0300

    ajuste

--- app/routes.py
+++ app/routes.py
@@ -6,11 +6,3 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html')
-
-@app.route('/contato.html')
-def contato():
-    return render_template('contato.html')
-
-@app.route('/features.html')
-def features():
-    return render_template('features.html')
