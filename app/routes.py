ec592c0bc45fb2f86259959667c9bf62ec25240f
anarobertaandrade <ana48945726@edum.rn.senac.br>
2023-01-20 11:36:58 -0300

    projeto_atual

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,11 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
